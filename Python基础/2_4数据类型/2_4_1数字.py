c69c70c243c932ea12a48817eb6af3dcc91b884a
BigFatSnake <1040478513@qq.com>
2026-09-19 09:42:36 +0800

    2.4.1 数字

---
+++ Python基础/2_4数据类型/2_4_1数字.py
@@ -0,0 +1,37 @@
+age=15
+print(age)
+print(type(age))
+
+height=1.74
+print(height)
+print(type(height))
+
+flex=2+3j
+print(flex)
+print(type(flex))
+
+num=1.0
+print(num)
+print(type(num))
+
+# + - * /
+#整除// 取余%  幂运算**
+a=10
+b=3
+
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+num=3.0
+print(type(num))
+print(int(num))
+
+num2=5
+print(type(num2))
+print(float(num2))
+
+#除法结果永远是浮点数
+#整除只取整数部分
+#整数与浮点数运算结果成浮点数
